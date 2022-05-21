0a4ea5d38267436f5f442ee27424a326974290c7
adlanmsyariaty <adlanmsyariaty@gmail.com>
2022-05-22 00:58:04 +0700

    change socket broadcast

--- socketConfig.js
+++ socketConfig.js
@@ -16,7 +16,7 @@ io.on('connection', socket => {
   console.log(`User connected: ${socket.id}`)
 
   socket.on("send_message", (data) => {
-    io.emit("receive_message", data);
+    socket.broadcast.emit("receive_message", data);
   });
 
 });
