afc32ba49c19387cdc2214d1852bd15a7845bf5b
adlanmsyariaty <adlanmsyariaty@gmail.com>
2022-05-24 23:30:46 +0700

    server update for history (status patch)

--- services/app/routes/consultantRouter.js
+++ services/app/routes/consultantRouter.js
@@ -9,7 +9,7 @@ router.delete("/:id", consultantController.deleteConsultant);
 router.post("/login", consultantController.loginConsultant);
 router.use(consultantAuthentication)
 router.patch("/", consultantController.patchVideoCode);
-// router.patch("/status", consultantController.patchVideoCode);
+router.patch("/status", consultantController.patchStatus);
 router.get("/histories/close", historyController.fetchConsultantHistoriesClose);
 
 module.exports = router;
